f856ac20235210d6ebaea05dedb51faa9a445c15
Charlie <200533963@student.georgianc.on.ca>
2026-09-11 13:36:15 -0400

    Updated main.cpp
    Added in all class work

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -2,16 +2,57 @@
 #include "raymath.h"
 #include "raygui.h"
 
+#include <array>
+#include <vector>
+
+constexpr float BALL_RADIUS = 25.0f;
+constexpr Vector2 GRAVITY = { 0.0f, 100.0f };
+
 int main()
 {
     InitWindow(800, 800, "Game");
     InitAudioDevice();
     SetTargetFPS(60);
 
+    Rectangle ground;
+    ground.x = 0;
+    ground.y = 700;
+	ground.width = GetScreenWidth();
+    ground.height = 20.0f;
+
+    Rectangle platform;
+    platform.x = 100.0f;
+	platform.y = ground.y - 100.0f;
+    platform.width = 50.0f;
+    platform.height = 100.0f;
+
+    Vector2 ball_launch_position;
+    ball_launch_position.x = platform.x + BALL_RADIUS;
+    ball_launch_position.y = platform.y - BALL_RADIUS;
+
+    Vector2 ball_position = ball_launch_position;
+    Vector2 ball_velocity = Vector2Zeros;
+
+    float ball_gravity_scale = 0.0f;
+
     while (!WindowShouldClose())
     {
+        float dt = GetFrameTime();
+        if (IsKeyPressed(KEY_SPACE)) 
+        {
+            ball_velocity = Vector2Rotate(Vector2UnitX, -30.0f * DEG2RAD) * 200.0f;
+			ball_gravity_scale = 1.0f;
+        }
+        ball_velocity += GRAVITY * dt * ball_gravity_scale;
+		ball_position += ball_velocity * dt;
+        
         BeginDrawing();
         ClearBackground(WHITE);
+        DrawRectangleRec(ground, BEIGE);
+        DrawRectangleRec(platform, GRAY);
+
+        DrawCircleV(ball_position, BALL_RADIUS, BLUE);
+        DrawCircleV(ball_launch_position, BALL_RADIUS, DARKGRAY);
 
         DrawCircleV(GetMousePosition(), 20.0f, RED);
 
